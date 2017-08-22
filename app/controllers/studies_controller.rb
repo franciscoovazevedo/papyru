d6db1c29aaef78e7b7c500aa1626240f8b9c27e6
franciscoazevedo <franciscoovazevedo@gmail.com>
2017-08-22 18:20:39 +0100

    studies#index

---
+++ app/controllers/studies_controller.rb
@@ -0,0 +1,23 @@
+class StudiesController < ApplicationController
+  def index
+   @studies = Study.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
