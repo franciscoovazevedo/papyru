27230e7a8aa67e236fb902d9179ea749d6946e05
franciscoazevedo <franciscoovazevedo@gmail.com>
2017-08-22 12:26:41 +0100

    validations corrected, also some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,22 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# def fromMayorToAcronym(mayor)
-#   result = ""
-#   if mayor.split(' ').length > 1
-#     mayor.split(' ').each do |word|
-#       result += word.first
-#     end
-#   else
-#     result = mayor.split('').first(3).join
-#   end
-#   result.upcase!
-#   # p result
-# end
+def fromMayorToAcronym(mayor)
+  result = ""
+  if mayor.split(' ').length > 1
+    mayor.split(' ').each do |word|
+      result += word.first
+    end
+  else
+    result = mayor.split('').first(3).join
+  end
+  result.upcase!
+end
 
 # Seeds - first create the subjects
-# subject = ["Math", "Italian", "Portuguese", "Spanish", "Ruby", "Java", "Algebra"]
-# array_subjects.each { |subject| Subject.create(name: subject, acronym: fromMayorToAcronym(subject) }
+subject = ["Math", "Italian", "Portuguese", "Spanish", "Ruby", "Java", "Algebra"]
+subject.each { |subject| Subject.create(name: subject, acronym: fromMayorToAcronym(subject)) }
 # Create the Mayors
-# major =  ["Informatic Engineer", "Business and Computer Science", "Data Engineering", "Psycology"]
-# major.each { |mayor| Mayor.create(name: mayor, acronym: fromMayorToAcronym(mayor)) }
+major =  ["Informatic Engineer", "Business and Computer Science", "Data Engineering", "Psycology", "Finance and Strategy", "Sports Manangement"]
+major.each { |mayor| Mayor.create(name: mayor, acronym: fromMayorToAcronym(mayor)) }
+
+#
