e6aa6660c0528e8e618c860118290acc4fb10337
franciscoazevedo <franciscoovazevedo@gmail.com>
2017-08-24 19:40:12 +0100

    ALREADY ADDING NEW CHANNELS

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,4 +1,19 @@
 class ChannelsController < ApplicationController
+  def new
+    @study = Study.find(params[:study_id])
+    @channel = @study.channels.new
+  end
+
+  def create
+    @channel = Channel.new(channel_params)
+    @study = Study.find(params[:study_id])
+    @channel.study = @study
+    if @channel.save
+      redirect_to @channel
+    else
+      render :new
+    end
+  end
 
   def show
     @channel = Channel.find(params[:id])
@@ -7,4 +22,9 @@ class ChannelsController < ApplicationController
     @documents = @channel.documents
   end
 
+  private
+
+  def channel_params
+    params.require(:channel).permit(:name)
+  end
 end
