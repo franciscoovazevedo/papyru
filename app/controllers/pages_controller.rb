e5044ecbe56266bb6096e849e06bdce3c5415232
Manuel Sepulveda <manuel_sep@hotmail.com>
2017-08-28 15:50:44 +0100

    navbar links done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,15 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :help, :about_us, :why_papyru]
 
   def home
   end
 
   def help
+  end
+
+  def about_us
+  end
+
+  def why_papyru
   end
 end
