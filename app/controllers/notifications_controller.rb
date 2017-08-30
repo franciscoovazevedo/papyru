56668a7ccb19f900343e1780ed7d530885ae358b
franciscoazevedo <franciscoovazevedo@gmail.com>
2017-08-30 21:12:49 +0100

    archive page

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,15 +1,17 @@
-
 class NotificationsController < ApplicationController
 
   def index
     @notifications_unread = current_user.notifications.unread
+  end
+
+  def archive
     @notifications_read = current_user.notifications.read
   end
 
   def change_status
     Notification.find(params[:id]).change_status!
     respond_to do |format|
-      format.html { redirect_to notifications_path unless request.xhr? }
+      format.html { redirect_to notifications_path }
       format.js
     end
   end
